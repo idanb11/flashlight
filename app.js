c6910e919e4e4b99ebfd57ec329be07e215ce1dc
idan <idanb11@gmail.com>
2016-08-10 14:07:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,25 @@
  */
 
 var ElasticSearch = require('elasticsearch'),
-   conf          = require('./config'),
-   fbutil        = require('./lib/fbutil'),
-   PathMonitor   = require('./lib/PathMonitor'),
-   SearchQueue   = require('./lib/SearchQueue');
+  conf = require('./config'),
+  fbutil = require('./lib/fbutil'),
+  PathMonitor = require('./lib/PathMonitor'),
+  SearchQueue = require('./lib/SearchQueue');
+
+var escOptions = {
+  hosts: [{
+    host: conf.ES_HOST,
+    port: conf.ES_PORT,
+    auth: (conf.ES_USER && conf.ES_PASS) ? conf.ES_USER + ':' + conf.ES_PASS : null
+  }]
+};
+
+for (var attrname in conf.ES_OPTS) {
+  escOptions[attrname] = conf.ES_OPTS[attrname];
+}
 
 // connect to ElasticSearch
-var esc = new ElasticSearch.Client({
-  hosts: [
-    {
-      host: conf.ES_HOST,
-      port: conf.ES_PORT,
-      auth: (conf.ES_USER && conf.ES_PASS) ? conf.ES_USER + ':' + conf.ES_PASS : null
-    }  
-  ]
-});
+var esc = new ElasticSearch.Client(escOptions);
 
 console.log('Connected to ElasticSearch host %s:%s'.grey, conf.ES_HOST, conf.ES_PORT);
 
